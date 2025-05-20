29bf674036b16b9599f4d7b5efe7b8cbc3abed3a
Guilherme Shibuya <gyudishibuya@gmail.com>
2025-05-19 22:53:33 -0300

    main file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-from src.rnn.rnn import RNN
+from src.model.model import WeatherRNN
 import numpy as np
 import torch
+import pandas as pd
 
-
+data_train = pd.read_csv(
+    '../datasets/DailyDelhiClimateTrain.csv',
+    parse_dates=['date'],
+    index_col='date'
+)
