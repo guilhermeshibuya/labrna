297a645cc074c4d22a11c0a62303e558ca7166b7
Guilherme Shibuya <gyudishibuya@gmail.com>
2025-06-17 10:57:46 -0300

    hyperparameter optimization with optuna

--- src/model/model.py
+++ src/model/model.py
@@ -3,20 +3,6 @@ from typing import Literal
 import torch
 import torch.nn as nn
 
-# class WeatherRNN(nn.Module):
-#     def __init__(self, input_size, hidden_size, num_layers):
-#         super(WeatherRNN, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.num_layers = num_layers
-#         self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-#         self.fc = nn.Linear(hidden_size, 1)
-#
-#     def forward(self, x):
-#         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-#         output, _ = self.rnn(x, h0)
-#         output = self.fc(output[:, -1, :])
-#         return output
-
 
 class RNN(nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int, num_layers: int, activation: Literal['tanh', 'sigmoid', 'relu'] = 'tanh'):
---
+++ src/utils/optimize.py
@@ -0,0 +1,74 @@
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import optuna
+from typing import Literal
+import pandas as pd
+
+from model.model import RNN
+from utils.data import DailyClimateDataset
+from utils.seed import define_seed
+from utils.trainer import Trainer
+
+
+def objective(trial):
+    df_train = pd.read_csv(
+        '../../datasets/DailyDelhiClimateTrain.csv',
+        parse_dates=['date'],
+        index_col='date'
+    )
+
+    df_test = pd.read_csv(
+        '../../datasets/DailyDelhiClimateTest.csv',
+        parse_dates=['date'],
+        index_col='date'
+    )
+
+    df_train.drop(columns=['meanpressure'], inplace=True)
+    df_test.drop(columns=['meanpressure'], inplace=True)
+
+    define_seed(42)
+
+    val_ratio = 0.2
+    val_size = int(len(df_train) * val_ratio)
+    train_part = df_train[:-val_size]
+    val_part = df_train[-val_size:]
+
+    sequence_length = trial.suggest_int("sequence_length", 7, 63, step=7)
+    batch_size = trial.suggest_categorical("batch_size", [1, 2, 4, 8, 16, 32])
+
+    train_dataset = DailyClimateDataset(train_part, window_size=sequence_length)
+    val_dataset = DailyClimateDataset(val_part, window_size=sequence_length, scaler=train_dataset.scaler)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size)
+
+    input_size = train_dataset.X.shape[2]
+    output_size = 1
+    patience = 20
+    epochs = 300
+
+    hidden_size = trial.suggest_int("hidden_size", 8, 128)
+    num_layers = trial.suggest_int("num_layers", 1, 6)
+    activation = trial.suggest_categorical("activation", ["tanh", "sigmoid", "relu"])
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-1, log=True)
+
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size)
+
+    model = RNN(input_size, hidden_size, output_size, num_layers, activation)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    loss_fn = nn.MSELoss()
+
+    trainer = Trainer(model, optimizer, loss_fn)
+    _, history = trainer.train(train_loader, val_loader, patience, epochs)
+
+    val_losses = [entry['val_loss'] for entry in history]
+    return min(val_losses)
+
+
+study = optuna.create_study(direction="minimize")
+study.optimize(objective, n_trials=50)
+
+print("Best Hyperparameters:", study.best_params)
